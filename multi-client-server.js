7abbb853769759a217a14d7bce911cbbfd5eae63
Sander-Benten <66199145+Sander-Benten@users.noreply.github.com>
2020-06-16 11:18:04 +0200

    Add files via upload

---
+++ multi-client-server.js
@@ -0,0 +1,41 @@
+// server.js
+
+const WebSocket = require('ws')  // Include the ws package
+
+var transmittingClients = []  // To store transmitting connections
+var receivingClients = []    // To store receiving connections
+
+const wss = new WebSocket.Server({ port: 8080 })  // wss = a new WebSocketServer on port 8080
+
+wss.on('connection', client => {  // Define behavior of the server
+  console.log("client connected with IP => " + client._socket.remoteAddress) // Print the IP of the connected client
+
+  client.on('message', message =>{  // When the server receives a message
+
+    if (message == "transmitter"){  // If the client identifies itself as transmitter
+      transmittingClients.push(client)          // Save the transmitting client
+      client.send("succesfully registered as transmitting client")
+
+    }else if (message == "receiver") {  // If the client identifies itself as receiver
+      receivingClients.push(client)                 // Save the receiving client
+      client.send("succesfully registered as receiving client")
+
+    }else if (transmittingClients.includes(client)) {  // If a message comes from a transmitting client
+      receivingClients.forEach(function(value){
+        value.send(message)
+      })
+      
+    }else{  // If the message does not come from a transmitter and is not an identification
+      client.send("ERROR: cannot handle message")
+
+    }
+
+  })
+
+  client.on('close', function(reasonCode, description) {  // get's called when a client disconnects
+        console.log("a client disconnected")
+        console.log(reasonCode)
+        console.log(description)
+    });
+
+})
